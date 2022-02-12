6a57e54ae86518f8be2b970f7e7db549d98bb861
me <me@me.com>
2022-02-11 21:55:59 -0500

    Part5

--- blog/views.py
+++ blog/views.py
@@ -1,23 +1,9 @@
 from django.shortcuts import render
-
-posts = [
-    {
-        'author': 'Tutor',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'Feburary 10 2022'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'Feburary 11 2022'
-    }
-]
+from .models import Post
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
